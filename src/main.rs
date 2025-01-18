20c5bb9732bba4bfad2565d44d204f7de2cfdadb
Raf Gemmail <rafiq@dreamthought.com>
2025-01-18 17:44:15 +1300

    flake - rustup flake

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+  return eframe::run_native(
+    "scranner",
+    eframe::NativeOptions::default(),
+    Box::new(|_| Ok(Box::new(MyApp::default()))),
+  )
+}
+
+struct MyApp {
+  name: String,
+}
+
+impl Default for MyApp {
+  fn default() -> Self {
+    Self {
+      name: String::from("User"),
+    }
+  }
+}
+
+impl eframe::App for MyApp {
+  fn update(&mut self, ctx: &egui::Context, _:&mut eframe::Frame) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+      if ui.button("Scan").clicked() {
+        println!("presssed");
+      };
+    });
+    return;
+  }
 }
