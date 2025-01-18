32386e3277e860588ccc896d3e1d97fe7e1dd801
Raf Gemmail <rafiq@dreamthought.com>
2025-01-19 08:36:05 +1300

    format - rustlinted

--- src/main.rs
+++ src/main.rs
@@ -5,66 +5,78 @@ use std::thread;
 mod scranner;
 
 fn main() -> Result<(), eframe::Error> {
-  eframe::run_native(
-    "Packet Sniffer",
-    eframe::NativeOptions::default(),
-    Box::new(|_| Ok(Box::new(MyApp::default()))),
-  )
+    eframe::run_native(
+        "Packet Sniffer",
+        eframe::NativeOptions::default(),
+        Box::new(|_| Ok(Box::new(MyApp::default()))),
+    )
 }
 
 struct MyApp {
-  results: Arc<Mutex<Vec<String>>>,
-  scanning: Arc<Mutex<bool>>,
+    packets: Arc<Mutex<Vec<String>>>,
+    is_scanning: Arc<Mutex<bool>>,
 }
 
 impl Default for MyApp {
-  fn default() -> Self {
-    Self {
-      results: Arc::new(Mutex::new(Vec::new())),
-      scanning: Arc::new(Mutex::new(false)),
+    fn default() -> Self {
+        Self {
+            packets: Arc::new(Mutex::new(Vec::new())),
+            is_scanning: Arc::new(Mutex::new(false)),
+        }
     }
-  }
 }
 
 impl eframe::App for MyApp {
-  fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-    egui::CentralPanel::default().show(ctx, |ui| {
-      let scanning = *self.scanning.lock().unwrap();
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let is_scanning = *self.is_scanning.lock().unwrap();
 
-      if !scanning && ui.button("Start").clicked() {
-        let results = Arc::clone(&self.results);
-        let scanning_flag = Arc::clone(&self.scanning);
-        *scanning_flag.lock().unwrap() = true;
+            // Start button
+            if !is_scanning && ui.button("Start").clicked() {
+                println!("cloning packets");
+                let packets = Arc::clone(&self.packets);
+                let is_scanning = Arc::clone(&self.is_scanning);
 
-        thread::spawn(move || {
-          if let Ok(packets) = scranner::sniff("enp7s0".to_string()) {
-            for packet in packets {
-              if !*scanning_flag.lock().unwrap() {
-                break;
-              }
-              let packet_info = format!(
-                "Src: {}:{} -> Dst: {}:{}",
-                packet.src_ip, packet.src_port, packet.dst_ip, packet.dst_port
-              );
-              results.lock().unwrap().push(packet_info);
+                // Start scanning in a separate thread
+                *is_scanning.lock().unwrap() = true;
+                thread::spawn(move || {
+                    println!("Spawned");
+                    if let Ok(captured_packets) = scranner::sniff("enp7s0".to_string()) {
+                        for packet in captured_packets {
+                            println!("packet captured");
+                            if !*is_scanning.lock().unwrap() {
+                                break;
+                            }
+                            let packet_info = format!(
+                                "Src: {}:{} -> Dst: {}:{}",
+                                packet.src_ip, packet.src_port, packet.dst_ip, packet.dst_port
+                            );
+                            println!("Adding a packet {}", packet_info.to_string());
+                            packets.lock().unwrap().push(packet_info);
+                        }
+                    }
+                });
             }
-          }
+
+            // Stop button
+            if is_scanning && ui.button("Stop").clicked() {
+                *self.is_scanning.lock().unwrap() = false;
+            }
+
+            // Separator and scroll area for packets
+            ui.separator();
+            ui.label("packets");
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.label("packet area");
+                for packet in self.packets.lock().unwrap().iter().rev() {
+                    println!("Got a packet");
+                    ui.label(packet);
+                }
+            });
+            ui.separator();
         });
-      }
 
-      if scanning && ui.button("Stop").clicked() {
-        *self.scanning.lock().unwrap() = false;
-      }
 
-      ui.separator();
-      egui::ScrollArea::vertical().show(ui, |ui| {
-        for packet in self.results.lock().unwrap().iter().rev() {
-          ui.label(packet);
-        }
-      });
-    });
-
-    ctx.request_repaint();
-  }
+        ctx.request_repaint();
+    }
 }
-
---
+++ src/scranner.rs
@@ -0,0 +1,45 @@
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::{self, NetworkInterface};
+use pnet::packet::ethernet::EthernetPacket;
+use std::error::Error;
+
+pub struct PacketInfo {
+    pub src_ip: String,
+    pub dst_ip: String,
+    pub src_port: u16,
+    pub dst_port: u16,
+}
+
+pub fn sniff(ifname: String) -> Result<Vec<PacketInfo>, Box<dyn Error>> {
+    println!("Sniffing interface {}", ifname);
+    let interface_name_match = |iface: &NetworkInterface| iface.name == ifname;
+    let interfaces = datalink::interfaces();
+    let interface = match interfaces.into_iter().find(interface_name_match) {
+        Some(iface) => iface,
+        None => return Err(format!("Interface {} not found", ifname).into()),
+    };
+
+    let mut rx = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(_, rx)) => rx,
+        Ok(_) => return Err("Unhandled channel type".into()),
+        Err(e) => return Err(Box::new(e)),
+    };
+
+    let mut packets = Vec::<PacketInfo>::new();
+
+    while let Ok(packet) = rx.next() {
+        if let Some(eth_packet) = EthernetPacket::new(packet) {
+            println!("Captured Ethernet packet: {:?}", eth_packet);
+
+            packets.push(PacketInfo {
+                src_ip: eth_packet.get_source().to_string(),
+                dst_ip: eth_packet.get_destination().to_string(),
+                src_port: 0,
+                dst_port: 0,
+            });
+        } else {
+            eprintln!("Error parsing Ethernet packet");
+        }
+    }
+    Ok(packets)
+}
