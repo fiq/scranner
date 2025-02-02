6b052ed9fc1baf40d45a288a1c62b741d3adee05
Raf Gemmail <rafiq@dreamthought.com>
2025-02-02 19:04:31 +1300

    refactor - ipv4 specific parsing function

--- src/scranner.rs
+++ src/scranner.rs
@@ -42,37 +42,7 @@ pub fn sniff(ifname: String, sample_size: i32) -> Result<Vec<PacketInfo>, Box<dy
             println!("Captured Ethernet packet: {:?}", eth_packet);
 
             if eth_packet.get_ethertype() == EtherTypes::Ipv4 {
-                if let Some(ip_packet) = Ipv4Packet::new(eth_packet.payload()) {
-                    let src_ip_v4 = ip_packet.get_source().to_string();
-                    let dst_ip_v4 = ip_packet.get_destination().to_string();
-                    let (src_port, dst_port) = match ip_packet.get_next_level_protocol() {
-                        pnet::packet::ip::IpNextHeaderProtocols::Tcp => {
-                            if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
-                                (tcp_packet.get_source(), tcp_packet.get_destination())
-                            } else {
-                                (0, 0)
-                            }
-                        },
-                        pnet::packet::ip::IpNextHeaderProtocols::Udp => {
-                            if let Some(udp_packet) = UdpPacket::new(ip_packet.payload()) {
-                                (udp_packet.get_source(), udp_packet.get_destination())
-                            } else {
-                                (0, 0)
-                            }
-                        },
-                        _ => (0, 0),
-                    };
-
-                    packets.push(PacketInfo {
-                        date: Utc::now(),
-                        src_mac: eth_packet.get_source().to_string(),
-                        dst_mac: eth_packet.get_destination().to_string(),
-                        src_ip_v4,
-                        dst_ip_v4,
-                        src_port,
-                        dst_port,
-                    });
-                }
+                process_ipv4_packet(&mut packets, &eth_packet);
             } else {
                 packets.push(PacketInfo {
                     date: Utc::now(),
@@ -89,4 +59,40 @@ pub fn sniff(ifname: String, sample_size: i32) -> Result<Vec<PacketInfo>, Box<dy
         }
     }
     Ok(packets)
+}
+
+fn process_ipv4_packet(packets: &mut Vec<PacketInfo>, eth_packet: &EthernetPacket) {
+    if let Some(ip_packet) = Ipv4Packet::new(eth_packet.payload()) {
+        let src_ip_v4 = ip_packet.get_source().to_string();
+        let dst_ip_v4 = ip_packet.get_destination().to_string();
+
+        // FIXME: add port parsing in its own function for use with ipv6
+        let (src_port, dst_port) = match ip_packet.get_next_level_protocol() {
+            pnet::packet::ip::IpNextHeaderProtocols::Tcp => {
+                if let Some(tcp_packet) = TcpPacket::new(ip_packet.payload()) {
+                    (tcp_packet.get_source(), tcp_packet.get_destination())
+                } else {
+                    (0, 0)
+                }
+            },
+            pnet::packet::ip::IpNextHeaderProtocols::Udp => {
+                if let Some(udp_packet) = UdpPacket::new(ip_packet.payload()) {
+                    (udp_packet.get_source(), udp_packet.get_destination())
+                } else {
+                    (0, 0)
+                }
+            },
+            _ => (0, 0),
+        };
+
+        packets.push(PacketInfo {
+            date: Utc::now(),
+            src_mac: eth_packet.get_source().to_string(),
+            dst_mac: eth_packet.get_destination().to_string(),
+            src_ip_v4,
+            dst_ip_v4,
+            src_port,
+            dst_port,
+        });
+    }
 }
